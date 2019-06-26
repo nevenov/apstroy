513e0d070c05eb9a3479a13f922f4c7d4cb605a9
Stoyan Nedev <nevenov@abv.bg>
2019-06-26 12:44:56 +0300

    First commit

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароли должны содержать не менее шести символов и соответствовать подтверждению.',
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'Мы отправили вам ссылку для сброса пароля по электронной почте!',
+    'user' => "Мы не можем найти пользователя с таким адресом электронной почты.",
+
+];
